a9260a62bfe3ef16b2238905d5d3ee3f14ed50d2
Ariel Alvarado <alvarado.ariel.contact@gmail.com>
2018-04-21 15:13:45 -0400

    agregando ruta de estado

--- src/server.js
+++ src/server.js
@@ -8,21 +8,10 @@ logger.info('-> Cargando middlewares globales antes de las rutas'); // eslint-di
 
 logger.info('-> Cargando rutas'); // eslint-disable-line
 
-app.use('*', (req, res) => {
-  Noticia.find({
-    titulo: 'algo',
-  }).exec()
-    .then((resp) => {
-      res.json(resp);
-    })
-    .catch((err) => {
-      console.log('+++++++++++++++++++++++++++++++++++++++++++');
-      console.log(err);
-      console.log('+++++++++++++++++++++++++++++++++++++++++++');
-      res.status(500).json({
-        estado: 'ok',
-      });
-    });
+app.use('/estado', (req, res) => {
+  res.json({
+    estado: 'ok',
+  });
 });
 
 logger.info('-> Cargando middlewares globales despues de las rutas'); // eslint-disable-line
